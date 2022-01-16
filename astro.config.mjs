3a67b456e23b25fd781d8e740272aaa36c956aef
Tadeu Zagallo <tzagallo@apple.com>
2022-01-16 22:25:58 +0100

    Remove all the leftover template bits and start adding some initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,9 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Enable the Preact renderer to support Preact JSX components.
 	renderers: [],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
